89cea2dc094e246657ab73722cce6a1600411fb2
Emre Sahin <e.sahin6677@gmail.com>
2024-04-01 13:15:23 +0200

    finished chapter 12: mutating data

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -16,9 +16,8 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
- 
-  // NOTE: comment in this code when you get to this point in the course
 
+  // NOTE: comment in this code when you get to this point in the course
   const allPages = generatePagination(currentPage, totalPages);
 
   return (
